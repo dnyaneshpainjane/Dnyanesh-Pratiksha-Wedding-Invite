5ac0b38aef5341e2cadf1b830f1c12e4451e2165
Dnyanesh Painjane <81969659+dnyaneshpainjane@users.noreply.github.com>
2026-06-06 12:28:25 +0530

    Update door-opening.tsx

--- components/door-opening.tsx
+++ components/door-opening.tsx
@@ -33,7 +33,7 @@ export function DoorOpening({ onClose }: DoorOpeningProps) {
     timeoutRef.current = setTimeout(() => {
       setHideIntro(true)
       onClose()
-    }, 5000)
+    }, 5500)
   }
 
   const closeIntro = () => {
